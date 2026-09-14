d7ff1290063aa09f0b26592830f691801b5464b8
quickpots <102370841+quickpots@users.noreply.github.com>
2026-09-15 00:34:00 +0100

    Update Start.ps1

--- ps/Start.ps1
+++ ps/Start.ps1
@@ -1,3 +1,4 @@
+```powershell
 Clear-Host
 $host.ui.RawUI.WindowTitle = "Basic Checks - https://discord.gg/DvdWgcpbkp"
 
@@ -8,8 +9,7 @@ Write-Host @"
  | |  | | | | | |/ __| |/ /  ___/ _ \| __/ __|  \___ \\___ \ 
  | |__| | |_| | | (__|   <| |  | (_) | |_\__ \  ____) |___) |
   \___\_\\__,_|_|\___|_|\_\_|   \___/ \__|___/ |_____/_____/ 
-                                                                              
-
+                                                             
 "@ -ForegroundColor Cyan
 
 Write-Host "Made by QuickPots. DM @mcvitiesbiscuit for assistance" -ForegroundColor Yellow
@@ -17,6 +17,10 @@ timeout /t 5 | out-null
 
 
 # admin check
+
+Write-Host ""
+Write-Host "[ ADMIN CHECK ]" -ForegroundColor Cyan
+Write-Host ""
 
 $isAdmin = [System.Security.Principal.WindowsPrincipal]::new(
     [System.Security.Principal.WindowsIdentity]::GetCurrent()
@@ -31,6 +35,8 @@ if (-not $isAdmin) {
     Read-Host
     return
 }
+
+Write-Host "Administrator privileges confirmed." -ForegroundColor Green
 
 
 # event log functions
@@ -54,7 +60,6 @@ function Check-EventLog {
         Write-Host "$message logs were not found." -ForegroundColor Magenta
     }
 }
-
 
 function Check-RecentEventLog {
     param ($logName, $eventIDs, $message)
@@ -83,6 +88,10 @@ function Check-RecentEventLog {
 
 # pc boot time
 
+Write-Host ""
+Write-Host "[ PC BOOT TIME ]" -ForegroundColor Cyan
+Write-Host ""
+
 $lastBootTime = (Get-CimInstance -ClassName Win32_OperatingSystem).LastBootUpTime
 $formattedBootTime = $lastBootTime.ToString("yyyy-MM-dd hh:mm tt")
 
@@ -92,6 +101,10 @@ Write-Host $formattedBootTime -ForegroundColor Yellow
 
 # recycle bin
 
+Write-Host ""
+Write-Host "[ RECYCLE BIN ]" -ForegroundColor Cyan
+Write-Host ""
+
 $currentUserSID = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
 $recycleBinFolderPath = "C:\`$Recycle.Bin\$currentUserSID"
 
@@ -100,7 +113,7 @@ if (Test-Path -Path $recycleBinFolderPath) {
         $recycleBinFolder = Get-Item -Path $recycleBinFolderPath -Force
         $lastModifiedTime = $recycleBinFolder.LastWriteTime.ToString("MM/dd/yyyy hh:mm:ss tt")
 
-        Write-Host "RECYCLE BIN: " -NoNewline -ForegroundColor Cyan
+        Write-Host "LAST MODIFIED: " -NoNewline -ForegroundColor Cyan
         Write-Host $lastModifiedTime -ForegroundColor Yellow
     }
     catch {
@@ -111,17 +124,17 @@ else {
     Write-Host "Recycle Bin folder for the current user not found at $recycleBinFolderPath." -ForegroundColor Red
 }
 
-Write-Host ""
-
 
 # connected drives
+
+Write-Host ""
+Write-Host "[ CONNECTED DRIVES ]" -ForegroundColor Cyan
+Write-Host ""
 
 $drives = Get-CimInstance -ClassName Win32_LogicalDisk |
     Where-Object { $_.DriveType -ne 5 }
 
 if ($drives) {
-    Write-Host "Connected Drives:" -ForegroundColor Yellow
-
     foreach ($drive in $drives) {
         Write-Host "$($drive.DeviceID): $($drive.FileSystem)" -ForegroundColor Green
     }
@@ -130,12 +143,15 @@ else {
     Write-Host "No drives found." -ForegroundColor Red
 }
 
-Write-Host ""
-
 
 # services
 
+Write-Host ""
+Write-Host "[ SERVICES ]" -ForegroundColor Cyan
+Write-Host ""
+
 $services = 'SysMain','PcaSvc','DPS','EventLog','Schedule','WSearch','BAM','DusmSvc','Appinfo'
+
 $all = $services + ((Get-Service | Where-Object {
     $_.Name -like 'CDPUserSvc_*'
 }).Name)
@@ -144,7 +160,12 @@ $all | ForEach-Object {
 
     try {
         $s = Get-Service -Name $_ -ErrorAction Stop
-        $cfg = (Get-CimInstance -ClassName Win32_Service -Filter "Name='$($_)'").StartMode
+
+        $cfg = (
+            Get-CimInstance `
+                -ClassName Win32_Service `
+                -Filter "Name='$($_)'"
+        ).StartMode
 
         if ($cfg -eq 'Disabled') {
             Write-Host "$($_) : Disabled" -ForegroundColor Red
@@ -161,10 +182,12 @@ $all | ForEach-Object {
     }
 }
 
-Write-Host ""
-
 
 # windows settings
+
+Write-Host ""
+Write-Host "[ WINDOWS SETTINGS ]" -ForegroundColor Cyan
+Write-Host ""
 
 $settings = @(
     @{
@@ -207,12 +230,12 @@ foreach ($s in $settings) {
     }
 }
 
-Write-Host ""
-
 
 # event log checks
 
-Write-Host "Event Log Checks:" -ForegroundColor Yellow
+Write-Host ""
+Write-Host "[ EVENT LOG CHECKS ]" -ForegroundColor Cyan
+Write-Host ""
 
 Check-EventLog "Application" 3079 "USN Journal last deleted"
 Check-RecentEventLog "System" @(104, 1102) "Event Logs last cleared"
@@ -220,12 +243,12 @@ Check-EventLog "System" 1074 "User recent PC Shutdown"
 Check-EventLog "Security" 4616 "System time changed"
 Check-EventLog "System" 6005 "Event Log Service started"
 
-Write-Host ""
-
 
 # prefetch integrity
 
-Write-Host "Prefetch Files Integrity:" -ForegroundColor Yellow
+Write-Host ""
+Write-Host "[ PREFETCH FILES INTEGRITY ]" -ForegroundColor Cyan
+Write-Host ""
 
 $prefetchPath = "C:\Windows\Prefetch"
 
@@ -246,7 +269,6 @@ else {
     Write-Host "No hidden files found in Prefetch." -ForegroundColor Green
 }
 
-
 $readOnlyFiles = Get-ChildItem `
     -Path $prefetchPath `
     -Force `
@@ -264,27 +286,34 @@ else {
     Write-Host "No read-only files found in Prefetch." -ForegroundColor Green
 }
 
-Write-Host ""
-
 
 # vpn check
 
-Write-Host "VPN Check:" -ForegroundColor Yellow
+Write-Host ""
+Write-Host "[ VPN CHECK ]" -ForegroundColor Cyan
+Write-Host ""
 
 try {
-    $ip = (Invoke-WebRequest "https://ifconfig.me/ip" -UseBasicParsing).Content.Trim()
+    $ip = (
+        Invoke-WebRequest "https://ifconfig.me/ip"
+    ).Content.Trim()
 
     $proxyPath = "$env:TEMP\proxy.json"
 
-    (Invoke-WebRequest "https://proxycheck.io/v2/$ip`?vpn=1&asn=1" -UseBasicParsing).Content |
+    (
+        Invoke-WebRequest "https://proxycheck.io/v2/$ip`?vpn=1&asn=1"
+    ).Content |
         Set-Content $proxyPath
 
-    Get-Content $proxyPath | Select-String "proxy"
+    Get-Content $proxyPath |
+        Select-String "proxy"
 }
 catch {
     Write-Host "VPN check failed." -ForegroundColor Red
 }
 
 
+Write-Host ""
 Write-Host "Press enter to exit..." -ForegroundColor Yellow
 Read-Host
+```
